256cdb5e0259ffdee644186957a911492a1afff7
Eron Salihu <58089049+eronsalihu@users.noreply.github.com>
2021-02-07 12:33:31 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,16 +1,14 @@
-import optparse, random, re, string, urllib, urllib.parse, urllib.request
-from tkinter import *
 import tkinter as tk
+from tkinter import *
 from tkinter import ttk
 from tkinter.messagebox import askyesno
 import requests
 from pprint import pprint
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
+import pprint,optparse, random, re, string, urllib, urllib.parse, urllib.request
 from patterns import REGULAR_PATTERNS
 from patterns import DOM_PATTERNS
-
-VERSION ="0.3c"
 
 SMALLER_CHAR_POOL    = ('<', '>')                                                           # characters used for XSS tampering of parameter values (smaller set - for avoiding possible SQLi errors)
 LARGER_CHAR_POOL     = ('\'', '"', '>', '<', ';')                                           # characters used for XSS tampering of parameter values (larger set)
@@ -48,7 +46,7 @@ def scan_page(url, data=None):
             current = url if phase is GET else (data or "")
             for match in re.finditer(r"((\A|[?&])(?P<parameter>[\w\[\]]+)=)(?P<value>[^&#]*)", current):
                 found, usable = False, True
-                print("* scanning %s parameter '%s'" % (phase, match.group("parameter")))
+                url_step_1 = str("* scanning %s parameter '%s'" % (phase, match.group("parameter")))
                 prefix, suffix = ("".join(random.sample(string.ascii_lowercase, PREFIX_SUFFIX_LENGTH)) for i in range(2))
                 for pool in (LARGER_CHAR_POOL, SMALLER_CHAR_POOL):
                     if not found:
@@ -60,14 +58,14 @@ def scan_page(url, data=None):
                                 context = re.search(regex % {"chars": re.escape(sample.group(0))}, filtered, re.I)
                                 if context and not found and sample.group(1).strip():
                                     if _contains(sample.group(1), condition):
-                                        print(" (i) %s parameter '%s' appears to be XSS vulnerable (%s)" % (phase, match.group("parameter"), info % dict((("filtering", "no" if all(char in sample.group(1) for char in LARGER_CHAR_POOL) else "some"),))))
+                                        url_step_2 = str(" (i) %s parameter '%s' appears to be XSS vulnerable (%s)" % (phase, match.group("parameter"), info % dict((("filtering", "no" if all(char in sample.group(1) for char in LARGER_CHAR_POOL) else "some"),))))
                                         found = retval = True
                                     break
         if not usable:
             print(" (x) no usable GET/POST parameters found")
     except KeyboardInterrupt:
         print("\r (x) Ctrl-C pressed")
-    return retval
+    return (url_step_1+ '\n' + url_step_2)
 
 
 if __name__ == "__main__":
@@ -78,12 +76,22 @@ if __name__ == "__main__":
     options, _ = parser.parse_args()
 
 
+
+def space(Frame):
+    blank_label = tk.Label(Frame)
+    blank_label.configure(bg = "#252526",padx = 10, pady = 10, height = 2, fg = "white")
+    blank_label.pack( side = RIGHT)
+
+
 def get_all_forms(url):
+    """Given a `url`, it returns all forms from the HTML content"""
     soup = bs(requests.get(url).content, "html.parser")
     return soup.find_all("form")
 
 def get_form_details(form):
-
+    """
+    This function extracts all possible useful information about an HTML `form`
+    """
     details = {}
     # get the form action (target url)
     action = form.attrs.get("action").lower()
@@ -102,6 +110,7 @@ def get_form_details(form):
     return details
 
 def submit_form(form_details, url, value):
+    
     # construct the full URL (if the url provided in action is relative)
     target_url = urljoin(url, form_details["action"])
     # get the inputs
@@ -114,6 +123,8 @@ def submit_form(form_details, url, value):
         input_name = input.get("name")
         input_value = input.get("value")
         if input_name and input_value:
+            # if input name and value are not None,
+            # then add them to the data of form submission
             data[input_name] = input_value
 
     if form_details["method"] == "post":
@@ -123,10 +134,10 @@ def submit_form(form_details, url, value):
         return requests.get(target_url, params=data)
 
 def scan_xss(url):
-
+   
     # get all the forms from the URL
     forms = get_all_forms(url)
-    print(f"[+] Detected {len(forms)} forms on {url}.")
+    step_1 = str(f"[+] Detected {len(forms)} forms on {url}.")
     js_script = "<Script>alert('hi')</scripT>"
     # returning value
     is_vulnerable = False
@@ -135,49 +146,84 @@ def scan_xss(url):
         form_details = get_form_details(form)
         content = submit_form(form_details, url, js_script).content.decode()
         if js_script in content:
-            print(f"[+] XSS Detected on {url}")
-            print(f"[*] Form details:")
-            pprint(form_details)
+            step_2 = str(f"[+] XSS Detected on {url}")
+            step_3 = str(f"[*] Form details:")
+            step_4 = pprint.pformat(form_details)
             is_vulnerable = True
             # won't break because we want to print available vulnerable forms
-    return is_vulnerable
+    if True:        
+        return (step_1 +"\n" + step_2 + "\n" + step_3 + "\n"+step_4) 
+    else:
+        return "something went worng"
+
+ 
+window = tk.Tk()
+
+#navBar
+navBarFrame = Frame(master = window, width = 150, height = 650, bg =  "#252526")
+navBarFrame.pack(fill = X, side = tk.TOP, anchor = "w")
+
+space(navBarFrame)
+url_label = tk.Label(navBarFrame, text="URL")
+url_label.configure(bg = "#252526",padx = 10, pady = 10, width = 5, fg = "white",font =("Courier", 12))
+url_label.pack( side = LEFT)
 
 
-root= tk.Tk(className='XSS Testing sites')
 
-canvas1 = tk.Canvas(root, width = 600, height = 500, relief = 'raised')
-canvas1.pack()
+#mainFrame
+mainFrame = Frame(master = window, width = 650, height = 750, bg = "#1E1E1E")
+mainFrame.pack(fill = BOTH,anchor = "center")
 
-label1 = tk.Label(root, text='Testing Cross-Site Scripting')
-label1.config(font=('helvetica', 24))
-canvas1.create_window(300, 40, window=label1)
-
-label2 = tk.Label(root, text='Type your URL you want to test:')
-label2.config(font=('helvetica', 16))
-canvas1.create_window(300, 180, window=label2)
 
 input_text = StringVar()
-entry1 = ttk.Entry(root, textvariable = input_text, justify = CENTER)
-entry1.focus_force()
-entry1.pack(side = TOP, ipadx = 50, ipady = 10)
-canvas1.create_window(300, 240, window=entry1)
 
-def startTesting ():
-    url = entry1.get()
-    print(scan_xss(url))
+url_entry = tk.Entry(navBarFrame,textvariable = input_text)
+url_entry.configure(relief = RAISED, width = 50)
+url_entry.focus_force()
+url_entry.pack(side = LEFT)
 
-def urlTestingForms():
-    url = entry1.get()
+def formTesting ():
+    url = url_entry.get()
+    output_text = tk.Text(mainFrame )
+    output_text.config(font=('Courier', 16),fg = "white", bg = "#1e1e1e")
+    output_text.pack()
+
+    
+    txt = str(scan_xss(url)) 
+    output_text.insert(tk.END,txt)
+
+def ulrTesting():
+    url = url_entry.get()
     options.url = url
     result = scan_page(options.url if options.url.startswith("http") else "http://%s" % options.url, options.data)
-    print("\nscan results: %s vulnerabilities found" % ("possible" if result else "no"))
+    txt = str("\nscan results: %s vulnerabilities found" % ("possible" if result else "no"))  
+    txt_res = str(result)
+    text = txt_res + txt
+    output_text = tk.Text(mainFrame )
+    output_text.config(font=('Courier', 16),fg = "white", bg = "#1e1e1e")
+    output_text.pack()  
 
-button1 = tk.Button(text='XSS TEST', command=startTesting, bg='brown', fg='white', font=('helvetica', 9, 'bold'))
-button1.pack(side = TOP, pady = 10)
-canvas1.create_window(260, 270, window=button1)
+    # txt = str(scan_xss(url)) 
+    output_text.insert(tk.END,text)
+     
+dom_btn = tk.Button(navBarFrame, text = "DOM", command = formTesting, width = 10)
+dom_btn.configure(padx = 5, pady = 5, relief = RAISED,font =("Courier", 8))
+dom_btn.pack(side = RIGHT, anchor = "center")
 
-button2 = tk.Button(text='Url TEST', command=urlTestingForms, bg='brown', fg='white', font=('helvetica', 9, 'bold'))
-button2.pack(side = TOP, pady = 10)
-canvas1.create_window(340, 270, window=button2)
+# space(navBarFrame)
 
-root.mainloop()
+# form_btn = tk.Button(navBarFrame, text = "FORM", command = formTesting, width = 10)
+# form_btn.configure(padx = 5, pady = 5, relief = RAISED,font =("Courier", 8))
+# form_btn.pack(side = RIGHT, anchor = "center")
+
+space(navBarFrame)
+
+scan_btn = tk.Button(navBarFrame, text = "xSS Scan ", command = ulrTesting, width = 10,)
+scan_btn.configure(padx = 5, pady = 5, relief = RAISED,font =("Courier", 8))
+scan_btn.pack(side = RIGHT, anchor = "center")
+
+
+
+window.geometry("900x550")
+window.title("xSS Scanner")
+window.mainloop()
